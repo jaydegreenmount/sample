45f7b8808c230d24769a91bfd166db3ed0d76c36
Jayde Greenmount <46437941+jaydegreenmount@users.noreply.github.com>
2019-01-22 14:25:28 +0800

    Update db_credentials.php

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -2,7 +2,7 @@
 
 defined('DB_SERVER') ? null : define("DB_SERVER", "166.62.10.46");
 defined('DB_USER')   ? null : define("DB_USER", "websomnia");
-defined('DB_PASS')   ? null : define("DB_PASS", "websomnia");
+defined('DB_PASS')   ? null : define("DB_PASS", "*****");
 defined('DB_NAME')   ? null : define("DB_NAME", "game_studio");
 
 ?>
