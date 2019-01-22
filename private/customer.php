27cc63216d235eee70e7032fb1c4a39d289590f5
Jayde Greenmount <46437941+jaydegreenmount@users.noreply.github.com>
2019-01-22 14:23:52 +0800

    Update customer.php

--- private/customer.php
+++ private/customer.php
@@ -1,6 +1,6 @@
 <?php
 // If it's going to need the database, then it's 
-// probably smart to require it before we start.
+// require it before we start.
 
 class Customer extends DatabaseObject {
 	
